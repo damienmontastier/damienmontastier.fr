aab13af33c36ec0ba36188041d63b6a2c73dc48f
damienmontastier <montastier.damien@gmail.com>
2019-06-08 18:27:44 +0200

    add scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,33 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        damienmontastier.fr
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <p>Hey, my name is Damien. I studying actually at Gobelins l'Ecole de l'image in Interactive Dévelopement.</p>
+
+    <div id="projects">
+      <h5>Somes projects</h5>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    <div id="experiences">
+      <h5>Somes agencies</h5>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    <div id="contacts">
+      <h5>Contact</h5>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+// import AppLogo from '~/components/AppLogo.vue'
 
 export default {
   components: {
-    AppLogo
+    // AppLogo
   }
-}
+};
 </script>
 
 <style>
@@ -39,27 +37,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
 
